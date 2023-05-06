070fd772d5a9afaa7f5ed11965b27eb3245a1063
rukiye-41 <rukiye-elitok@hotmail.com>
2023-05-06 20:45:18 +0200

    V3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,5 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
         //v1 calismalarim...
         //v2 uzerinde calismalar
+        System.out.println("v3 kodlari ...");//v3
+
     }
 }
